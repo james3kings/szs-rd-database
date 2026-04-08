8b5db9f772cad3a5513367151850cdae4adf87e1
james3kings <jamesaldrin3kings@gmail.com>
2026-04-08 12:03:33 +0800

    Add files via upload

---
+++ my-website/script.js
@@ -0,0 +1,12 @@
+// Paste this in script.js
+function handleClick() {
+  alert("Welcome! Edit this in script.js");
+}
+
+document.querySelectorAll('a[href^="#"]').forEach(link => {
+  link.addEventListener('click', function (e) {
+    e.preventDefault();
+    const target = document.querySelector(this.getAttribute('href'));
+    if (target) target.scrollIntoView({ behavior: 'smooth' });
+  });
+});
